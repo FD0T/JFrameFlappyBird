95e84034bceb83d6e3ab2e29578df3c682a2998e
FD0T <vladik.fedotov@yandex.ru>
2021-11-20 22:09:13 +0300

    "Start menu" and "death screen" were added

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -3,11 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FlappyBird {
+public class FlappyBird extends JFrame {
 
     int vy = 0, vy_const = 75, ay_const = 30, score = 0;
     Timer timer;
-    JButton jump;
 
     public static void main(String[] args) {
         new FlappyBird();
@@ -17,10 +16,48 @@ public class FlappyBird {
 
     public void flappyBirdLoop(){
         JFrame flappyBirdWindow = new JFrame("JFrame Flappy Bird ");
-        flappyBirdWindow.setSize(480, 848);
         flappyBirdWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flappyBirdWindow.setSize(480, 848);
         flappyBirdWindow.setLayout(null);
         flappyBirdWindow.setVisible(true);
+
+
+        JPanel startMenu = new JPanel();
+        startMenu.setBackground(Color.blue);
+        startMenu.setBounds(0,0,480,848);
+        startMenu.setLayout(new GridLayout(1,1));
+        JButton startButton = new JButton("START");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    timer.start();
+                    startButton.setVisible(false);
+                    startMenu.setVisible(false);
+            }
+        });
+        startMenu.add(startButton);
+        flappyBirdWindow.add(startMenu);
+
+
+        JPanel deathMenu = new JPanel();
+        deathMenu.setBackground(Color.blue);
+        deathMenu.setBounds(0,0,480,848);
+        deathMenu.setLayout(new GridLayout(2,1));
+        deathMenu.setVisible(false);
+        JButton restart = new JButton("Restart");
+        restart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                timer.start();
+                deathMenu.setVisible(false);
+            }
+        });
+        JLabel scoreMenu = new JLabel();
+        scoreMenu.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+        scoreMenu.setText("Your score is " + score);
+        deathMenu.add(scoreMenu);
+        deathMenu.add(restart);
+        flappyBirdWindow.add(deathMenu);
 
         JLabel scoreOut = new JLabel();
         scoreOut.setFont(new Font("Comic Sans", Font.PLAIN, 64));
@@ -29,12 +66,12 @@ public class FlappyBird {
         flappyBirdWindow.add(scoreOut);
 
         JPanel bird = new JPanel();
-        bird.setBounds(100,400,10,10);
+        bird.setBounds(100,150,10,10);
         bird.setBackground(Color.black);
 
         flappyBirdWindow.add(bird);
 
-        jump = new JButton("Jump");
+        JButton jump = new JButton("Jump");
         jump.setBounds(190, 750, 100,50);
         jump.addActionListener(new ActionListener() {
             @Override
@@ -60,14 +97,29 @@ public class FlappyBird {
         timer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                jump.setEnabled(true);
                 vy = ay_const;
                 bird.setBounds(50,bird.getY()+vy, 10, 10);
                 if(bird.getY() >= 838 || bird.getY() <= 0){
-                    deathScreen();
+                    timer.stop();
+                    scoreMenu.setText("Your score is " + score);
+                    deathMenu.setVisible(true);
+                    score = 0;
+                    scoreOut.setText(String.valueOf(score));
+                    bird.setBounds(100,150,10,10);
+                    tubeU.setBounds(480, rand,50,500);
+                    tubeD.setBounds(480, tubeU.getY() + 500 + 140,50,500);
                 }
                 if((bird.getY() <= tubeU.getY() + tubeU.getHeight() || bird.getY() + bird.getHeight() >= tubeD.getY()) &&
                 bird.getX() + bird.getWidth() >= tubeU.getX() && bird.getX() <= tubeU.getX() + tubeU.getWidth()){
-                    deathScreen();
+                    timer.stop();
+                    scoreMenu.setText("Your score is " + score);
+                    deathMenu.setVisible(true);
+                    score = 0;
+                    scoreOut.setText(String.valueOf(score));
+                    bird.setBounds(100,150,10,10);
+                    tubeU.setBounds(480, rand,50,500);
+                    tubeD.setBounds(480, tubeU.getY() + 500 + 140,50,500);
                 }
 
                 if(tubeU.getX() <= -50 && tubeD.getX() <= -50){
@@ -83,12 +135,5 @@ public class FlappyBird {
                 }
             }
         });
-        timer.start();
-    }
-
-    public void deathScreen()
-    {
-        timer.stop();
-        jump.setEnabled(false);
     }
 }
